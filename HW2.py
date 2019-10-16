8fa613ac1772def0dbd7c9d3a7b63a5a41966e04
Phoenix1696 <emanuelegugliandolo96@gmail.com>
2019-10-17 01:07:35 +0200

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,26 +1,32 @@
-#Homework 2 Excersice 1
 import json
 import pandas as pd
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
-eng = pd.read_json(r"C:\Users\emanu\Desktop\HW2\Datasets\matches\matches_England.json")
-week = 0
+eng = pd.read_json(r"C:\Users\39335\Downloads\Data\matches_England.json")
+eng = eng.sort_values(by = "gameweek")
 teams = []
 score = []
-ranking = defaultdict(int)
-for i in eng["label"]:
-    teams.append(i.split(",")[0])
-    score.append(i.split(",")[1])
-for i in range(len(teams) - 1, -1, -1):
-    if int(score[i].split("-")[0]) > int(score[i].split("-")[1]):
-        ranking[teams[i].split("-")[0]] += 3
-    elif int(score[i].split("-")[0]) < int(score[i].split("-")[1]):
-        ranking[teams[i].split("-")[1]] += 3
-    elif int(score[i].split("-")[0]) == int(score[i].split("-")[1]):
-        ranking[teams[i].split("-")[0]] += 1
-        ranking[teams[i].split("-")[1]] += 1
-
-    week += 1
-    plt.plot(week, int(ranking[teams[i].split("-")[0]]))
-    plt.plot(week, int(ranking[teams[i].split("-")[1]]))
+ranking = dict()
+for result in eng["label"]:
+    teams.append(result.split(",")[0])
+    score.append(result.split(",")[1])
+# Creo due liste (Team1 - Team2, Score1 - Score2) ordinate per settimana
+for i in range(len(teams)):
+    if teams[i].split("-")[0] in ranking:
+        if teams[i].split("-")[1] in ranking:
+            if int(score[i].split("-")[0]) > int(score[i].split("-")[1]):
+                ranking[teams[i].split("-")[0]].append(ranking[teams[i].split("-")[0]][-1] + 3)
+                ranking[teams[i].split("-")[1]].append(ranking[teams[i].split("-")[1]][-1])
+            elif int(score[i].split("-")[0]) < int(score[i].split("-")[1]):
+                ranking[teams[i].split("-")[1]].append(ranking[teams[i].split("-")[1]][-1] + 3)
+                ranking[teams[i].split("-")[0]].append(ranking[teams[i].split("-")[0]][-1])
+            elif int(score[i].split("-")[0]) == int(score[i].split("-")[1]):
+                ranking[teams[i].split("-")[1]].append(ranking[teams[i].split("-")[1]][-1] + 3)
+                ranking[teams[i].split("-")[0]].append(ranking[teams[i].split("-")[0]][-1] + 3)
+        else:
+            ranking[teams[i].split("-")[1]] = [0]
+    else:
+        ranking[teams[i].split("-")[0]] = [0]
+for keys in ranking:
+    plt.plot(ranking[keys])
