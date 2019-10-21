225981424f3bb7ba48ed7409f59fc6b3f8a8dda7
Phoenix1696 <emanuelegugliandolo96@gmail.com>
2019-10-22 00:07:50 +0200

    Update RQ4.py

--- RQ4.py
+++ RQ4.py
@@ -1,15 +1,24 @@
 import json
 import pandas
-from collections import defaultdict
-import matplotlib.pyplot as plt
+from operator import itemgetter
 
-events_England = pandas.read_json(r"C:\Users\emanu\Downloads\Data\events\events_England.json")
+events_England = pandas.read_json(r"C:\Users\39335\Downloads\Data\events_England.json")
 is_passes = events_England.eventName == "Pass"
 events_England = events_England[is_passes]
 events_England = events_England.reset_index()
 events_England.drop(columns = ["index"], inplace = True)
 TopPlyPass = dict()
-for j in range(len(events_England["tags"])):
-    for i in range(len(events_England["tags"][i])):
-        if (1801 == events_England["tags"][j][i]["id"]):
+for i in range(len(events_England["tags"])):
+    for j in range(len(events_England["tags"][i])):
+        if (1801 == events_England["tags"][i][j]["id"]):
             TopPlyPass.setdefault(events_England["playerId"][i], [0, 0])[0] += 1
+        if (1802 == events_England["tags"][i][j]["id"]):
+            TopPlyPass.setdefault(events_England["playerId"][i], [0, 0])[1] += 1
+
+Top10 = []
+for key in list(TopPlyPass.keys()):
+    if (TopPlyPass[key][0] + TopPlyPass[key][1] < 100):     # Set treshold
+        del TopPlyPass[key]
+    else:
+        Top10.append([key, TopPlyPass[key][0]/(TopPlyPass[key][0]+TopPlyPass[key][1])])
+print(sorted(Top10, key = itemgetter(1), reverse = True)[:10])
