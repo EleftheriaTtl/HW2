0d37616b790181658208c827a6b6d9d37b6a6041
Phoenix1696 <emanuelegugliandolo96@gmail.com>
2019-10-28 21:45:54 +0100

    Update CRQ1.py

--- CRQ1.py
+++ CRQ1.py
@@ -3,70 +3,32 @@ import pandas as pd
 from operator import itemgetter
 import matplotlib.pyplot as plt
 
-events_England = pandas.read_json(r"C:\Users\39335\Downloads\Data\events_England.json")
+events_England = pd.read_json(r"C:\Users\39335\Downloads\Data\events_England.json")
 events_England = events_England.reset_index()
-events_England.drop(columns=["index"], inplace=True)
-TeamGoals = dict()  # Teams ID as value, a list of the goals for each time interval
-PlayerGoals = dict()  # Players ID as value, a list of the goals for each time interval
-TotalGoals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # A list of the sum of all goals for each time interval
+events_England.drop(columns = ["index"], inplace = True)
+TeamGoals = dict()   # Teams ID as value, a list of the goals for each time interval
+PlayerGoals = dict() # Players ID as value, a list of the goals for each time interval
+TotalGoals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # A list of the sum of all goals for each time interval
 for i in range(len(events_England["tags"])):
     for j in range(len(events_England["tags"][i])):
-        # For each event if the tag is 101 (a goal) I increase the count of a list depending of the minute it was scored.
-        # There is a different list for each team, each player and the total sum of the goals.
+    # For each event if the tag is 101 (a goal) I increase the count of a list depending of the minute it was scored.
+    # There is a different list for each team, each player and the total sum of the goals.
         if (101 == events_England["tags"][i][j]["id"]):
-            if (0 <= events_England["eventSec"][i] <= 540 and events_England["matchPeriod"][i] == "1H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[0] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[0] += 1
-                TotalGoals[0] += 1
-            if (541 <= events_England["eventSec"][i] <= 1080 and events_England["matchPeriod"][i] == "1H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[1] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[1] += 1
-                TotalGoals[1] += 1
-            if (1081 <= events_England["eventSec"][i] <= 1620 and events_England["matchPeriod"][i] == "1H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[2] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[2] += 1
-                TotalGoals[2] += 1
-            if (1621 <= events_England["eventSec"][i] <= 2160 and events_England["matchPeriod"][i] == "1H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[3] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[3] += 1
-                TotalGoals[3] += 1
-            if (2161 <= events_England["eventSec"][i] <= 2700 and events_England["matchPeriod"][i] == "1H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[4] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[4] += 1
-                TotalGoals[4] += 1
-            if (2701 <= events_England["eventSec"][i] and events_England["matchPeriod"][i] == "1H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[5] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[5] += 1
-                TotalGoals[5] += 1
-
-            if (0 <= events_England["eventSec"][i] <= 540 and events_England["matchPeriod"][i] == "2H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[6] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[6] += 1
-                TotalGoals[6] += 1
-            if (541 <= events_England["eventSec"][i] <= 1080 and events_England["matchPeriod"][i] == "2H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[7] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[7] += 1
-                TotalGoals[7] += 1
-            if (1081 <= events_England["eventSec"][i] <= 1620 and events_England["matchPeriod"][i] == "2H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[8] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[8] += 1
-                TotalGoals[8] += 1
-            if (1621 <= events_England["eventSec"][i] <= 2160 and events_England["matchPeriod"][i] == "2H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[9] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[9] += 1
-                TotalGoals[9] += 1
-            if (2161 <= events_England["eventSec"][i] <= 2700 and events_England["matchPeriod"][i] == "2H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[10] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[10] += 1
-                TotalGoals[10] += 1
-            if (2701 <= events_England["eventSec"][i] and events_England["matchPeriod"][i] == "2H"):
-                PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[11] += 1
-                TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[11] += 1
-                TotalGoals[11] += 1
+            for m in range(6):
+                if (m*540 <= events_England["eventSec"][i] <= (m+1)*540 and events_England["matchPeriod"][i] == "1H"):
+                    PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[m] += 1
+                    TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[m] += 1
+                    TotalGoals[m] += 1
+            for m in range(6):
+                if (m*540 <= events_England["eventSec"][i] <= (m+1)*540 and events_England["matchPeriod"][i] == "2H"):
+                    PlayerGoals.setdefault(events_England["playerId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[6+m] += 1
+                    TeamGoals.setdefault(events_England["teamId"][i], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])[6+m] += 1
+                    TotalGoals[6+m] += 1
 intervals = ["0-9", "9-18", "18-27", "27-36", "36-45", "45+", "45-54", "54-63", "63-72", "72-81", "81-90", "90+"]
 
 ######## I ########
-plt.bar(intervals, TotalGoals)
+plt.barh(intervals, TotalGoals, color = "green")
+plt.title('Goals scored for each time interval')
 
 ######## II ########
 TopScore = []   # List of team ID and goal scored in the interval 81-90
@@ -84,7 +46,7 @@ Top10.drop("teamId", axis = 1, inplace = True)
 print(Top10.head(10))
 
 ######## III ########
-BestPlayers = []   # List of player with at least a goal in at least 8 different intervals.
+BestPlayers = []   # List of players with at least a goal in at least 8 different intervals.
 count = 0
 for key in PlayerGoals:
     for i in PlayerGoals[key]:
