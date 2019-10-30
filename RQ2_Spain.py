3475e4d961c6defa5fb6214179bf3582f4b775d8
luciatesta97 <56517946+luciatesta97@users.noreply.github.com>
2019-10-30 18:01:11 +0100

    Add files via upload

---
+++ RQ2_Spain.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas 
+
+
+
+import datetime
+
+
+
+from dateutil.parser import parse
+
+
+
+from collections import defaultdict
+
+
+
+import matplotlib.pyplot as plt
+
+
+
+import scipy.stats as scs
+
+
+
+#--------------------------------------------------------------------------------
+
+
+
+#read my file
+
+
+
+
+
+eng = pandas.read_json(r"C:\Users\lucy_\Downloads\matches\matches_Spain.json")
+
+
+
+teams = []
+
+
+
+score = []
+
+
+
+ranking = dict()
+
+
+
+for result in eng["label"]:
+
+
+
+    teams.append(result.split(",")[0].strip())
+
+
+
+    score.append(result.split(",")[1].strip())
+
+
+
+matchteam = []
+
+
+
+matchscore = []
+
+
+
+for i in range(len(teams)):
+
+
+
+    o = teams[i].split("-")
+
+
+
+    p = score[i].split("-")
+
+
+
+    matchteam.append([o[0].strip(), o[1].strip()])
+
+
+
+    matchscore.append([p[0].strip(), p[1].strip()])
+
+
+
+def cont(myteam):
+
+
+
+    winhome = 0
+
+
+
+    winaway = 0
+
+
+
+    drawhome = 0
+
+
+
+    drawaway = 0
+
+
+
+    losehome = 0
+
+
+
+    loseaway = 0
+
+
+
+    for i in range(len(eng)):
+
+
+
+        if myteam==matchteam[i][0]:
+
+
+
+            if matchscore[i][0]>matchscore[i][1]:
+
+
+
+                winhome +=1
+
+
+
+            elif matchscore[i][0]<matchscore[i][1]:
+
+
+
+                losehome +=1
+
+
+
+            else:
+
+
+
+                drawhome +=1
+
+
+
+        elif myteam==matchteam[i][1]:
+
+
+
+            if matchscore[i][0]>matchscore[i][1]:
+
+
+
+                loseaway +=1
+
+
+
+            elif matchscore[i][0]<matchscore[i][1]:
+
+
+
+                winaway +=1
+
+
+
+            else:
+
+
+
+                drawaway +=1
+
+
+
+    lst = [[winhome,winaway], [losehome, loseaway], [drawhome, drawaway]]
+
+
+
+    Contigency_Table = pandas.DataFrame(lst)
+
+
+
+    Contigency_Table.columns = ["Home", "Away"]
+
+
+
+    Contigency_Table.rename(index = {0:'''Win''', 1:'''Lose''', 2:'''Draw'''}, inplace = True)
+
+
+
+    return Contigency_Table
+
+
+
+
+
+print("Real Madrid")
+
+print(cont('Real Madrid'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Barcelona")
+
+print(cont('Barcelona'))
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+print("Athletic Bilbao")
+
+print(cont('Athletic Bilbao'))
+
+
+
+
+
+# In[5]:
+
+
+scs.chi2_contingency(cont('Real Madrid'))
+
