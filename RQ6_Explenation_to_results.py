9e2bcc500e79236a691254dd703d9b331bc9790a
luciatesta97 <56517946+luciatesta97@users.noreply.github.com>
2019-10-30 18:16:43 +0100

    Add files via upload

---
+++ RQ6_Explenation_to_results.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+###from the analisys of RQ6 we can notice that for every League the strongest nation is the league's nations itself. 
+###This is due to the fact that avery nation has more own players that foreign ones. 
+
